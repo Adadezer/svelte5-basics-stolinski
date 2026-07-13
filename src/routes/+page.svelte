775da3ef326bbd57d24b75f2ba8f555563578248
Adadezer <adadezer.iwazaki@veredaeducacao.com>
2026-07-13 12:10:12 -0300

    feat: example: toggle status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
 
 	let name = $state('Iwazaki');
+
+	let status: 'ABERTA' | 'FECHADA' = $state('ABERTA');
+
+	function toggle() {
+		status = status === 'ABERTA' ? 'FECHADA' : 'ABERTA';
+	}
 </script>
 
 <Header {name} />
 
 <label for="name">Nome:</label>
 <input id="name" type="text" bind:value={name} />
+
+<p>Está loja agora está {status}</p>
+<button onclick={toggle}>Toggle status</button>
