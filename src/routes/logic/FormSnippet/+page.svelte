6613714dde82872e0424f9e23be5183715e90ab3
Adadezer <adadezer.iwazaki@veredaeducacao.com>
2026-07-14 16:09:31 -0300

    feat: example: #snippet

--- src/routes/logic/FormSnippet/+page.svelte
+++ src/routes/logic/FormSnippet/+page.svelte
@@ -10,12 +10,19 @@
 </script>
 
 <main class="container">
+	<!-- Snippet children (implícito, passado para outro componente. O h3 é o children de HeaderFormSnippet) -->
 	<HeaderFormSnippet name={formState.name}>
 		<h3>Olá {formState.name}!</h3>
+
+		<!-- Snippet nomeado com argumento (secondChild) -->
+		{#snippet secondChild(sobrenome)}
+			Sobrenome: {sobrenome}
+		{/snippet}
 	</HeaderFormSnippet>
 
 	<p>Etapa: {formState.step + 1}</p>
 
+	<!--  Snippet local (declarado e usado no mesmo arquivo) -->
 	{@render formStep({ question: 'Qual é o seu nome', id: 'name', type: 'text' })}
 
 	<!-- aqui o id é tipado com keyof, do tipo formState. O typescript entende que o id tem que ser um dos keys de formState (name, birthday, step, error) -->
@@ -36,9 +43,3 @@
 		</article>
 	{/snippet}
 </main>
-
-<style>
-	.error {
-		color: red;
-	}
-</style>
